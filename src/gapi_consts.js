33df22f550c30d11694a82ec58150eeb76a57174
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2019-05-27 19:41:54 +0200

    release-build for github pages

--- src/gapi_consts.js
+++ src/gapi_consts.js
@@ -1,13 +1,15 @@
 // App client id and api key
-export const CLIENT_ID = 'temporarily redacted';
+export const CLIENT_ID = '779271503066-2a5r51l1etd7b6e7a8iks0acqh8lfutp.apps.googleusercontent.com';
 //export const API_KEY = '---';
 
 // Array of API discovery doc URLs for APIs
 export const DISCOVERY_DOCS = [ 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest' ];
 
 // Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
-export const SCOPES = 'https://www.googleapis.com/auth/drive'
-                   //+ ' https://www.googleapis.com/auth/drive.file'
-                   //+ ' https://www.googleapis.com/auth/drive.readonly'
-                   + ' https://www.googleapis.com/auth/drive.appdata'
-                   + '' //'https://www.googleapis.com/auth/drive.install';
+export const SCOPES = [
+    // 'https://www.googleapis.com/auth/drive',
+    // 'https://www.googleapis.com/auth/drive.readonly',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive.appdata',
+    'https://www.googleapis.com/auth/drive.install'
+].join(' ');
